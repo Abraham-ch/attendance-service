604164c8e88c24d491c5bde95937689d9009e12f
Abraham-ch <abraham.chafloque.a@uni.pe>
2026-08-12 23:37:13 -0500

    feat: create_user handler

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,8 +1,9 @@
 use axum::{Json, extract::{Path, State}};
+use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::schema::user::{Role, UpdateUser, User};
+use crate::schema::user::{NewUser, Role, UpdateUser, User};
 
 #[axum::debug_handler]
 pub async fn list_users(State(pool): State<PgPool>) -> Json<Vec<User>>{ 
@@ -28,7 +29,61 @@ pub async fn list_users(State(pool): State<PgPool>) -> Json<Vec<User>>{
   Json(full_users)
 }
 
-pub async fn create_user() -> &'static str { "Create user" }
+#[axum::debug_handler]
+pub async fn create_user(State(pool): State<PgPool>, Json(new_user): Json<NewUser>) -> Json<User> {
+
+    let user = User{
+      id: Uuid::new_v4(),
+      first_name: new_user.first_name,
+      last_name: new_user.last_name,
+      email: new_user.email,
+      avatar: new_user.avatar,
+      role: new_user.role,
+      created_at: Utc::now(),
+      updated_at: Utc::now()
+    };
+
+    let create_user: User = sqlx::query_as!(
+      User, 
+      r#"
+          INSERT INTO users
+              (
+                  id,
+                  first_name,
+                  last_name,
+                  email,
+                  avatar,
+                  role,
+                  created_at,
+                  updated_at
+              )
+          VALUES
+              ($1, $2, $3, $4, $5, $6, $7, $8)
+          RETURNING
+              id,
+              first_name,
+              last_name,
+              email,
+              avatar,
+              role AS "role: Role",
+              created_at,
+              updated_at
+      "#,
+      user.id,
+      user.first_name,
+      user.last_name,
+      user.email,
+      user.avatar,
+      user.role as Role,
+      user.created_at,
+      user.updated_at
+      )
+      .fetch_one(&pool)
+      .await
+      .expect("Failed to create a new user.");
+
+    Json(create_user)
+}
 
 #[axum::debug_handler]
 pub async fn get_user_by_id(State(pool): State<PgPool>, Path(id): Path<Uuid>) -> Json<User> {
--- src/schema/user.rs
+++ src/schema/user.rs
@@ -3,25 +3,13 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug)]
-pub struct NewUser<'a>{
-  pub id: &'a Uuid,
-  pub first_name: &'a String,
-  pub last_name: &'a String,
-  pub email: &'a String,
-  pub avatar: &'a String,
-  pub role: &'a String,
-  pub created_at: &'a DateTime<Utc>,
-  pub updated_at: &'a DateTime<Utc> 
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug)]
-pub struct NewUserHandler{
+#[derive(FromRow, Debug, Deserialize, Serialize)]
+pub struct NewUser{
   pub first_name: String,
   pub last_name: String,
   pub email: String,
   pub avatar: String,
-  pub role: String,
+  pub role: Role,
 }
 
 #[derive(FromRow, Debug, Deserialize, Serialize)]
